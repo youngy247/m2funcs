e2422a3ad6bf3fecb7dc8e3deee871d3d9eaf598
Adam Young <adamyoungy678@gmail.com>
2023-03-21 16:11:19 +0000

    adding parameters to functions

--- userDefFunc.php
+++ userDefFunc.php
@@ -2,4 +2,12 @@
 
 echo '<h1>User-defined Functions</h1>';
 
-sayHello();
+function sayHello($name, $age)
+{
+    echo 'Hello, ' . $name . '! You are ' . $age . '.';
+}
+
+sayHello('adam', 21);
+echo '<p>&nbsp;</p>';
+echo '<p></p>';
+sayHello('george', 65);
